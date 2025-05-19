e80e1085c8ea2adc1590c593a57903bbf979e557
mark.ku <mark.ku@ibuypower.com>
2025-05-19 23:07:17 +0800

    update application

--- member/src/main/java/com/example/MemberApplication.java
+++ member/src/main/java/com/example/MemberApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+@ComponentScan("com.common.member")
 public class MemberApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class, args);
